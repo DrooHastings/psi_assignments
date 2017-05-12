2acbda9e5ffffeb27180fb180fbe9b5ddbeb0218
Andrew Hastings <droo.hastings@gmail.com>
2017-05-12 17:03:08 -0500

    search feature working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,7 +3,10 @@ var myApp =angular.module( 'myApp', [] );
 myApp.controller( 'AssignmentController', function($http){
   console.log('NG');
 
+  //globals
   var vm = this;
+  vm.items = [];
+
 
 
 
@@ -22,10 +25,49 @@ myApp.controller( 'AssignmentController', function($http){
       url: '/assignment',
       data: objectToSend
     }).then(function(response){
+      console.log('back from server with:', response);
+      vm.getAssignments();
       // vm.getItems();
       //empty inputs
     });
-};//end addAssignment
+  };
+
+    vm.getAssignments = function (){
+      console.log('in getAssignments');
+      $http({
+        method: 'GET',
+        url: '/assignment'
+      }).then(function success(response){
+        console.log('response.data is:', response.data);
+        vm.items = response.data;
+      });//end http
+    };//end getAssignments
+
+    vm.deleteAssn = function (id){
+      console.log('in getAssnID');
+      $http({
+        method: 'DELETE',
+        url: '/assignment/'+ id,
+      }).then(function success(response){
+        vm.getAssignments();
+      });//end http
+    };//end getAssignments
+
+    vm.getAssn = function (name){
+      console.log('in get assn', name);
+      var route = '/assignment/' + name;
+      console.log(route);
+      $http({
+        method: 'GET',
+        url: route,
+      }).then(function success(response){
+        console.log('search response: ', response.data);
+        vm.items = response.data;
+        // vm.getAssignments();
+      });//end then
+    };//end getAssn
+
+
 
 
 });//end myApp
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var assignSchema = mongoose.Schema ({
 });
 
 //model
-var homeworks = mongoose.model('homeworks', assignSchema );
+var things = mongoose.model('things', assignSchema );
 
 //uses
 app.use(express.static('public'));
@@ -25,11 +25,44 @@ app.use(bodyParser.urlencoded( {extended: true }) );
 //port
 var port = process.env.PORT || 8888;
 
+app.post('/assignment', function(req, res){
+  console.log('in POST route');
+  console.log(req.body);
+  var newAssign = things(req.body);
+  newAssign.save().then(function(){
+  res.sendStatus(200);
+});// end save to db
+});// end post
+
+app.get('/assignment/:name?', function(req, res){
+  console.log('in GET route');
+  console.log(req.params);
+  if (req.params.name === undefined) {
+    things.find().then(function (data){
+      console.log('name undefined');
+      res.send(data);
+    }); // end then
+  } else {
+      things.find({student: req.params.name}).then(function (data){
+        console.log(data);
+        res.send(data);
+    });//end then
+  }//end else
+});// end get
+
+app.delete('/assignment/:id',function(req, res){
+  console.log('req.param to delete:', req.params.id);
+  things.remove({_id: req.params.id}).then(function(){
+      res.sendStatus(200);
+    });//end remove
+});//end app.delete
+
+
+
 //spin up server
 app.listen(port, function(){
   console.log('server up on:', port);
-
-});
+});//end server
 
 app.get('/', function(req, res){
   res.sendFile(path.resolve('public/views/index.html') );
